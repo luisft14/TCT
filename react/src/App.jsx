87181605df2c63cc042912882356aa35da7dbd98
luisft14 <luisf3462@gmail.com>
2023-09-27 19:26:22 -0600

    modulos laravel

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+function App() {
+
+  return (
+    <>
+      <h1>Bienvenido a CTC</h1>
+      <button className="btn btn-primary">Enviar</button>
+    </>
+  )
+}
+
+export default App
